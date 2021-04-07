b29d714699cb48a83e5ac4344cfb6d4224a17b99
Niek Palm <npalm@users.noreply.github.com>
2021-04-07 14:35:36 +0200

    feat: cleanup actions with post hook (#13)
    * feat: cleanup actions with post hook
    
    * chore(ci): Updating dist

---
+++ src/cleanup.ts
@@ -0,0 +1,27 @@
+import * as core from '@actions/core';
+import * as fs from 'fs';
+import * as path from 'path';
+
+async function cleanup(): Promise<void> {
+  const baseDir = core.getInput('base_dir', { required: true });
+  const githubWorkspacePath = process.env['GITHUB_WORKSPACE'] || '';
+  const baseDirPath = path.join(githubWorkspacePath, baseDir);
+
+  try {
+    await new Promise((resolve, reject) => {
+      core.info(`Removing private / inner source actions dir: ${baseDir}`);
+      fs.rmdir(baseDirPath, { recursive: true }, (error) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(true);
+        }
+      });
+    });
+  } catch (e) {
+    core.debug(e);
+    core.setFailed(e.message);
+  }
+}
+
+cleanup();
